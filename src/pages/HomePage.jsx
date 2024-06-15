56768a19b7ccb90953b0faecf62d6886bd54b58b
HT-35 <huyfa352002@gmail.com>
2024-06-15 10:39:18 +0700

    save

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,28 @@
-import { auth } from "firebase-app/firebase-config";
-import { signOut } from "firebase/auth";
 import React from "react";
 
 const HomePage = () => {
-  const handleSignOut = () => {
-    signOut(auth);
-  };
-
   return (
-    <button onClick={handleSignOut} className="px-4 py-2 bg-slate-300">
-      Đăng Xuất
-    </button>
+    <div className="max-w-screen-2xl mx-auto w-full">
+      <header className="nav flex justify-between lg:text-2xl max-sm:text-base py-2 ">
+        <div className="nav flex justify-center items-center gap-8 max-sm:gap-3">
+          <img src="./img/monkey.png" alt="" srcset="" className="w-10" />
+          <span>Home</span>
+          <span>Blog</span>
+          <span>Contact</span>
+        </div>
+        <div className="search flex justify-between gap-6">
+          <input
+            type="text"
+            name=""
+            id=""
+            className="px-2 py-4 border-[1px] border-[#CFCFCF] rounded-lg"
+          />
+          <button className="bg-[#00B4AA] px-2 py-4 rounded-lg">Sign Up</button>
+        </div>
+      </header>
+      <main></main>
+      <footer></footer>
+    </div>
   );
 };
 
